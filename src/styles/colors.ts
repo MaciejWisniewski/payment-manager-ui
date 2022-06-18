73c339897b3018140ba67d6bedbb1db96b0797a6
Maciej Wisniewski <matthias.wski@gmail.com>
2022-06-19 00:05:34 +0200

    Add styles and components folders. Set Circular Pro as body font-family

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  purple: {
+    60: '#4C00C2',
+    90: '#32007E',
+    100: '#3B058E',
+  },
+  background: '#E5E5E5',
+  white: '#FFFFFF',
+  grey: {
+    20: `#D3D8E1`,
+    50: `#798291`,
+    70: `#444E5D`,
+    90: `#1A212C`,
+  },
+  green: {
+    30: `#19AC51`,
+  },
+  red: {
+    30: `#FC484C`,
+  },
+};
